8f0a2024bac9fade45aa775cefdc83f42178eaeb
Akira0x01 <akira0x01@gmail.com>
2024-10-08 22:34:44 +0800

    feat: Update create-validator-account.sh to fix hex address extraction
    The commit updates the create-validator-account.sh script to fix the extraction of the hex address. Previously, the script was failing to extract the hex address due to incorrect file path handling. This fix ensures that the hex address is correctly extracted and appended to the validator_address file.

--- windows/share/create-validator-account.sh
+++ windows/share/create-validator-account.sh
@@ -15,5 +15,5 @@ $CHAIND keys add validator --keyring-backend test --home $DATA_DIR --output json
 # export account address to .vcity/validator_address
 validator_address_file="$HOME/.vcity/validator_address"
 $CHAIND debug addr $($CHAIND keys show validator -a --keyring-backend test --home $DATA_DIR) > $validator_address_file
-hex_address=$(grep "Address hex:" $validator_address_file | awk -F'[: ]' '{print $3}')")
+hex_address=$(grep "Address hex:" "$validator_address_file" | awk -F'[: ]' '{print $3}')
 $CHAIND debug addr $hex_address >> $validator_address_file
