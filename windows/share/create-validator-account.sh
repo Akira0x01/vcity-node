081bc6ab2cd50844a328b8724b36548001f72b5a
Akira0x01 <akira0x01@gmail.com>
2024-10-08 22:31:08 +0800

    feat: Create validator account and address files in the specified directory
    This commit adds a script to create a validator account and address files in the specified directory. The script generates a new key for the validator and exports the account address to the validator_address file. Additionally, it retrieves the hex address and appends it to the validator_address file.
    
    Recent user commits:
    - chore: Update docker-compose.yaml to copy all files from the share directory
    - feat: Add scripts to create validator account and validator
    - chore: Update npm dependencies and add web3 package
    
    Recent repository commits:
    - add backend
    - add backend
    - update
    - update test-node
    - fix node data persistant issuer
    - add create-validator.sh
    - fix seeds

--- windows/share/create-validator-account.sh
+++ windows/share/create-validator-account.sh
@@ -8,8 +8,12 @@ DATA_DIR="/home/user/.vcity"
 CHAINID="vcitychain_20230825-1"
 DENOM_UNIT="uvcity"
 
+validator_key_file="$HOME/.vcity/validator_key"
 # create new key
-$CHAIND keys add validator --keyring-backend test --home $DATA_DIR --output json > "$HOME/.vcity/validator_key"
+$CHAIND keys add validator --keyring-backend test --home $DATA_DIR --output json > $validator_key_file
 
 # export account address to .vcity/validator_address
-$CHAIND debug addr $($CHAIND keys show validator -a --keyring-backend test --home $DATA_DIR) > "$HOME/.vcity/validator_address"
+validator_address_file="$HOME/.vcity/validator_address"
+$CHAIND debug addr $($CHAIND keys show validator -a --keyring-backend test --home $DATA_DIR) > $validator_address_file
+hex_address=$(grep "Address hex:" $validator_address_file | awk -F'[: ]' '{print $3}')")
+$CHAIND debug addr $hex_address >> $validator_address_file
